1c7ee61e9ec28997e9a64a109a39e42ae820b57b
ata1635 <atakan_berk.rana@hotmail.com>
2025-01-04 18:13:56 +0100

    Added  the sprite atlas

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,21 +1,38 @@
 package main;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
-
 	private GameScreen gameScreen;
+	private BufferedImage img;
 	
 	public Game() {
-		setSize(400, 400);
+		importImg();
+		
+		setSize(640, 640);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setTitle("Tower Defense");
+		
 		gameScreen = new GameScreen();
-		add(gameScreen);
+		add(gameScreen); //method is used to add a component (like a button, text field, label, etc.) to a container (like a frame, panel, or applet).
 	}
 	
+	private void importImg() {
+		InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		Game game = new Game();
 	}
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,19 +2,37 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel{
 	
+	private Random random;
+	
 	public GameScreen() {
-		
+		random = new Random();
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.RED);
-		g.fillRect(50, 50, 100, 100);
+		for(int x = 0; x < 20; x++) {
+//			g.setColor(getRandomColor());
+//			g.fillRect(i * 32, 0, 32, 32);
+			for(int y = 0; y < 20; y++) {
+				g.setColor(getRandomColor());
+				g.fillRect(x * 32, y * 32, 32, 32);
+			}
+			
+		}
+
+	}
+	
+	private Color getRandomColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		return new Color(r,g,b);
 	}
 	
 }
